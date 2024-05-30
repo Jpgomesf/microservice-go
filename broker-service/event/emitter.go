30bfca54e833b7f730a78989a68a7d2811230bcc
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-05-29 23:19:11 -0300

    :construction: Work in progress broker-service emitter
    Basic queue service

---
+++ broker-service/event/emitter.go
@@ -0,0 +1,19 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Emitter struct {
+	connection *amqp.Connection
+}
+
+func (e *Emitter) setup() error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+	return declareExchange(channel)
+}
